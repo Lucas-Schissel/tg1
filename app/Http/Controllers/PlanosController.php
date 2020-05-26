21c888d1e4a44f7eecee87a48f13075ce2610a76
Lucas-Schissel <lucas.schissel@gmail.com>
2020-05-26 14:39:19 -0300

    5º Envio - Funçao Adicionar

--- app/Http/Controllers/PlanosController.php
+++ app/Http/Controllers/PlanosController.php
@@ -3,10 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Planos;
 
 class PlanosController extends Controller
 {
     function telaCadastro(){
+        return view('telas_cadastros.cadastro_plano');
+    }
+
+    function adicionar(Request $req){
+
+        $req->validate([
+            'nome' => 'required',
+            'descricao' => 'required',
+            'valor' => 'required|numeric',
+        ]);
+
+        $nome = $req->input('nome');
+        $descricao = $req->input('descricao');
+        $valor = $req->input('valor');
+    	
+
+            $pla = new Planos();
+            $pla->nome = $nome;
+            $pla->descricao = $descricao;
+            $pla->valor = $valor;
+
+            if ($pla->save()){
+                session([
+                    'mensagem' =>"Plano: $nome, foi adicionado com sucesso!",
+                    's'=>'s'
+                ]);
+            } else {
+                session([
+                    'mensagem' =>"Plano: $nome, nao foi adicionado!",
+                    'f'=>'f'
+                ]);
+            }
+            
         return view('telas_cadastros.cadastro_plano');
     }
 }
