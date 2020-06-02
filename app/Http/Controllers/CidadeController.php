4e27a963c7add3e536fae1eb908cd55090cec1d7
Lucas-Schissel <lucas.schissel@gmail.com>
2020-06-02 20:36:53 -0300

    11º Envio - funçao adicionar Cidade Controller e blade cidade

--- app/Http/Controllers/CidadeController.php
+++ app/Http/Controllers/CidadeController.php
@@ -12,4 +12,34 @@ class CidadeController extends Controller
         $estados = Estado::all();
         return view('telas_cadastros.cadastro_cidade' , [ 'estados' => $estados]);
     }
+
+    function adicionar(Request $req){
+
+        $req->validate([
+            'nome' => 'required|unique:cidades,nome',
+            'id_estado' => 'required|numeric',
+        ]);
+
+        $nome = $req->input('nome');
+        $id_estado = $req->input('id_estado');
+      
+
+            $cde = new Cidade();
+            $cde->nome = $nome;
+            $cde->id_estado = $id_estado;            
+
+            if ($cde->save()){
+                session([
+                    'mensagem' =>"Cidade: $nome, foi adicionada com sucesso!",
+                    's'=>'s'
+                ]);
+            } else {
+                session([
+                    'mensagem' =>"Cidade: $nome, nao foi adicionada!",
+                    'f'=>'f'
+                ]);
+            }
+
+        return redirect()->route('cidade_cadastro');
+    }
 }
--- resources/views/telas_cadastros/cadastro_cidade.blade.php
+++ resources/views/telas_cadastros/cadastro_cidade.blade.php
@@ -2,5 +2,54 @@
 @section('conteudo')
 <!-- Programar aqui-->
 
+<div class= "row">
+	<span class="d-block p-2 bg-dark text-center text-white w-100">
+		<h2>
+			Cadastro de Cidades
+			<i class="icon-download"></i>
+		</h2>
+	</span>
+</div>
+
+<div class="container">
+    <div class="row text-center p-2">
+
+        <div class="col-lg-2 col-md-0 col-sm-0 col-0">
+			<!-- coluna vazia esquerda -->
+		</div>
+
+        <div  class="col-lg-8 col-md-12 col-sm-12 col-12 mt-4 p-3 border border-success rounded">
+
+			<form method="post" action="{{ route('cidade_add') }}">
+			@csrf		
+                    <select name="id_estado" class="form-control mt-1 border border-success rounded" required>
+			        <option value="" disabled selected>Escolha uma estado:</option>
+			            @foreach ($estados as $e)
+			                <option value="{{ $e->id}}">{{$e->nome}}</option>
+			            @endforeach
+		            </select>
+
+                    <input class="form-control mt-1 border border-success rounded" type="text"  name="nome" placeholder="Digite um nome . . ." required>
+					
+					<button class="btn btn-success btn-block mt-1 "  type="submit">
+					 Cadastrar
+					<i class="icon-plus-circled"></i>
+					</button>			 
+			<form>  
+
+        </div>
+			
+		<div class="col-lg-2 col-md-0 col-sm-0 col-0">
+			<!-- coluna vazia direita -->
+		</div>
+
+	</div>
+</div> 
+
+<div class= "row">
+	<span class="d-block p-2 bg-dark w-100">
+	</span>
+</div>
+
 <!------------------->
 @endsection
