cd7698141acc88cc63f9fb6590e0e77ec28a5aeb
Lucas-Schissel <lucas.schissel@gmail.com>
2020-06-03 00:46:13 -0300

    1º Envio - confir model entrega

--- app/Entrega.php
+++ app/Entrega.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Entrega extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'entregas';
+    protected $primaryKey = 'id';
+
 }
