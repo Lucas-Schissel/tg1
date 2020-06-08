0786f5cd470c79a6c160f5ed4db58e6c943b0dea
EduardoSchwambach <eduardosilverio018@gmail.com>
2020-06-07 23:47:35 -0300

    index-v01

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,56 @@
 @extends('templates.principal')
 @section('conteudo')
-
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+    <style>
+    .card-img-top{
+        width:100%;
+        height:16rem;
+    }
+    .card{
+        margin:3%;
+    }
+    </style>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2017/03/13/17/26/ecommerce-2140604_960_720.jpg" class="img-fluid" alt="Imagem responsiva">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h5 class="card-title">Planos</h5>
+                        <p class="card-text">Clique aqui e conheça nossas ofertas e escolha a que mais combina com você</p>
+                            <a href="#" class="btn btn-primary">Conhecer</a>
                 </div>
-            </div>
         </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://cdn.pixabay.com/photo/2018/05/09/15/28/question-3385451_960_720.jpg" class="img-fluid" alt="Imagem responsiva">
+                <div class="card-body">
+                    <h5 class="card-title">Status</h5>
+                        <p class="card-text">Não sabe como esta o seu pedido? Clique aqui! Nós vamos te ajudar.</p>
+                            <a href="#" class="btn btn-primary">Acompanhar</a>
+                </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/12/20/05/24/store-1919713_960_720.png" class="img-fluid" alt="Imagem responsiva">
+        <div class="card-body">
+            <h5 class="card-title">Registrar Empresa</h5>
+                <p class="card-text">Aqui é possivel preencher os dados da sua Empresa para que possa aproveitar nossas soluções.</p>
+            <a href="#" class="btn btn-primary">Registrar</a>
+        </div>
+</div>
+<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/23/17/51/action-1854040_960_720.jpg" class="img-fluid" alt="Imagem responsiva">
+        <div class="card-body">
+            <h5 class="card-title">Lista de Motoboys</h5>
+                <p class="card-text">Aqui esta a lista de todos os motoboys onde é possivel ver quem são nossos associados.</p>
+            <a href="#" class="btn btn-primary">Lista Motoboy</a>
+        </div>
+</div>
+<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/04/25/21/14/woman-1353211_960_720.png" class="img-fluid" alt="Imagem responsiva">
+        <div class="card-body">
+            <h5 class="card-title">Configurações</h5>
+                <p class="card-text">Precisa atualizar algum dado? Ou conferir alguma permissão, esse é o lugar certo.</p>
+            <a href="#" class="btn btn-primary">Acessar as Configurações</a>
+        </div>
+</div>
     </div>
 </div>
 
