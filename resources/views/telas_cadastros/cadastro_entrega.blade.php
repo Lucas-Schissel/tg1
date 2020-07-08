1fce9258c27d47d195f441e66a9de6cc617cbba7
Lucas-Schissel <lucas.schissel@gmail.com>
2020-07-08 00:57:26 -0300

    1º Ajuste- cep com viacep

--- resources/views/telas_cadastros/cadastro_entrega.blade.php
+++ resources/views/telas_cadastros/cadastro_entrega.blade.php
@@ -43,11 +43,14 @@
 
                     <div class="col-lg-6 col-md-6 col-sm-6 col-12">
 
-                            <input id="cep" class="form-control m-1 border border-success rounded" type="text" name="cep" placeholder="Digite um cep . . ." value="{{old('cep')}}" required>
+                            <input id="cep" class="form-control m-1 border border-success rounded" type="text" name="cep" placeholder="Digite um cep . . ." value="{{old('cep')}}" required autocomplete="off">
                     
                     </div>
 
-                    <div class="col-lg-6 col-md-6 col-sm-6 col-0">        
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-12">  
+                    <button  class="btn btn-info btn-block mt-1"  onclick="carregaInformacoes()"> 
+                    Carregar informações 
+                    </button>	    
                     </div>
 
                     <div class="col-lg-6 col-md-6 col-sm-6 col-12">
@@ -66,7 +69,7 @@
 
                     <div class="col-lg-9 col-md-9 col-sm-9 col-12">
 
-                        <input id="rua" class="form-control m-1 border border-success rounded" type="text"  name="rua" placeholder="Digite uma rua . . ." value="{{old('rua')}}" required>
+                        <input id="logradouro" class="form-control m-1 border border-success rounded" type="text"  name="logradouro" placeholder="Digite uma rua . . ." value="{{old('rua')}}" required>
 
                     </div>
 
@@ -137,58 +140,37 @@
 	</span>
 </div>
 
-<script type="text/javascript" >
+<script type="text/javascript">
+    function carregaInformacoes(){
+        var cep, http;
+        cep = document.getElementById("cep").value;
 
-        $(document).ready(function() {
+        if(cep != ""){
 
-            function limpa_formulário_cep() {
+        http = new XMLHttpRequest();
+        http.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200 ){
+                var dados = JSON.parse(this.responseText);
+                document.getElementById("cidade").value = dados.localidade;
+                document.getElementById("bairro").value = dados.bairro;
+                document.getElementById("estado").value = dados.uf;
+                document.getElementById("logradouro").value = dados.logradouro;
+                document.getElementById("resposta").value ="";
 
-                $("#rua").val("");
-                $("#bairro").val("");
-                $("#cidade").val("");
-
+            }else if(this.readyState != 4){
+                
+                document.getElementById("resposta").innerHTML ="Carregando";
             }
+        }
 
-            $("#cep").blur(function() {
+        http.open("GET" , "http://viacep.com.br/ws/" + cep + "/json/");
+        http.send();
 
-                var cep = $(this).val().replace(/\D/g, '');
+        }
 
-                if (cep != "") {
+    }
 
-                    var validacep = /^[0-9]{8}$/;
-
-                    if(validacep.test(cep)) {
-
-                        $("#rua").val("...");
-                        $("#bairro").val("...");
-                        $("#cidade").val("...");
-
-                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
-
-                            if (!("erro" in dados)) {
-                                $("#rua").val(dados.logradouro);
-                                $("#bairro").val(dados.bairro);
-                                $("#cidade").val(dados.localidade);
-    
-                            }
-                            else {
-                                limpa_formulário_cep();
-                                alert("CEP não encontrado.");
-                            }
-                        });
-                    }
-                    else {
-                        limpa_formulário_cep();
-                        alert("Formato de CEP inválido.");
-                    }
-                } 
-                else {
-                    limpa_formulário_cep();
-                }
-            });
-        });
-
-    </script>
+</script>
 
 <!------------------->
 @endsection
